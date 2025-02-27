84b819536a45e0613fd11d123d63e9c7dcc41de5
eduk5r118 <eduardocoussirat@gmail.com>
2025-02-27 08:05:34 -0300

    introdução ao PHP

---
+++ aula01/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto{
+            color:yellow;
+        }
+    </style>
+    <body>
+    <p class="texto">
+        <?php 
+        echo "Olá mundo";
+        ?>
+    </p>
+    </body>
+</html>
