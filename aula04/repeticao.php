aea01355d9841f3160a208a8d85489ac5684501b
eduk5r118 <eduardocoussirat@gmail.com>
2025-03-19 10:33:57 -0300

    aula sobre loops

---
+++ aula04/repeticao.php
@@ -0,0 +1,26 @@
+<?php
+    $i = 1;
+
+    while ($i < 6){
+        echo $i;
+        $i++;
+        echo "<br>";
+    }
+
+    echo "__________";
+
+    $i < 6;
+
+    do{
+        echo $i;
+        $i++;
+        echo "<br>";
+    } while ($i < 6);
+
+    echo "-------------";
+
+    for ($x = 0; $x <= 10; $x++) {
+        echo "O número é: $x <br>";
+    }
+
+?>
