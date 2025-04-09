533507976aa0e0b3c1115ae19e5f3aac47cb81e9
eduk5r118 <eduardocoussirat@gmail.com>
2025-04-09 10:33:21 -0300

    aula sobre funções php

---
+++ aula05/funcao.php
@@ -0,0 +1,15 @@
+<?php
+    function myMessage() {
+        echo "Hello world" , "<br>";
+    }
+
+    myMessage();
+    myMessage();
+
+    function soma($n1, $n2){
+        echo $n1+$n2 , "<br>";
+    }
+
+    soma(3,4);
+    soma(3,10)
+?>
